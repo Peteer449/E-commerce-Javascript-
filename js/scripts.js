221a7208cfa3cb470aabd63384d90b0dea3e8c06
Pedro Esposito <pedro.esposito99@gmail.com>
2022-07-04 09:55:35 -0300

    Local storage para el carrito y modal para los juegos

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,47 @@
-const nombre=prompt("Pone tu nombre")
 const impuestoGanancias = 0.35
 const impuestoPais = 0.3
 let productos = [{
     id:1,
     juego:"Call of duty",
-    precio: 1200
+    precio: 1200,
+    descripcion:"Call of Duty® ofrece el descarnado realismo y la intensidad cinematográfica de las épicas batallas de la II Guerra Mundial como nunca antes: a través de los ojos de ciudadanos soldado y héroes olvidados de la alianza de naciones que ayudaron a conformar el curso de la historia moderna.",
+    foto:"./assets/imagenes/call of duty.jpg",
+    link:"https://store.steampowered.com/app/2620/Call_of_Duty/?l=spanish"
 },
 {
     id:2,
     juego:"Cyberpunk",
-    precio:2500
+    precio:2500,
+    descripcion:"Cyberpunk 2077 es un RPG de aventura y acción de mundo abierto ambientado en el futuro sombrío de Night City, una peligrosa megalópolis obsesionada con el poder, el glamur y las incesantes modificaciones corporales.",
+    foto:"./assets/imagenes/cyberpunk.jpeg",
+    link:"https://store.steampowered.com/app/1091500/Cyberpunk_2077/?l=spanish"
 },
 {
     id:3,
     juego:"Juanito y los clonosaurios",
-    precio:600
+    precio:600,
+    descripcion: "Un parque de diversiones del futuro donde hay dinosaurios que vuelven a la vida con avanzadas tecnicas de clonacion.",
+    foto:"./assets/imagenes/juanito y los clonosaurios.jpg",
+    link:"https://www.youtube.com/watch?v=g5HA-OQssEw"
 },
 {
     id:4,
     juego:"Putting challenge",
-    precio:200
+    precio:200,
+    descripcion: "ayy que lindo....",
+    foto:"./assets/imagenes/putting challenge.jpg",
+    link:"https://youtu.be/capTpivF8n0"
 },
 {
     id:5,
     juego:"God of war",
-    precio:5000
+    precio:5000,
+    descripcion:"Kratos ha dejado atrás su venganza contra los dioses del Olimpo y vive ahora como un hombre en los dominios de los dioses y monstruos nórdicos. En este mundo cruel e implacable debe luchar para sobrevivir… y enseñar a su hijo a hacerlo también.",
+    foto:"./assets/imagenes/god of war.jpg",
+    link:"https://store.steampowered.com/app/1593500/God_of_War/"
 }]
+const productosJSON=JSON.stringify(productos)
+localStorage.setItem("productos",productosJSON)
 
 
 
@@ -36,16 +52,22 @@ const carritoHTML= document.getElementById("carrito")
 const totalSteamHTML = document.getElementById("totalSteam")
 const totalRealHTML = document.getElementById("totalReal")
 const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
-let precioTotal = 0
-let precioTotalReal = 0
 let idDelCarrito = 0
+const carritoStorage = localStorage.getItem("carrito")
+let carrito = JSON.parse(carritoStorage) ?? {
+    precioTotal : 0,
+    precioTotalReal : 0,
+    cantidadDeProductos:0,
+    carritoDisplay:""
+}
 
 const agregarAlCarrito = (id) => {
     const producto = productos.find(element => element.id == id)
     idDelCarrito++
-    precioTotal += producto.precio
-    precioTotalReal += producto.precioReal
-    cantidadDeProductosHTML.innerHTML++
+    carrito.precioTotal += producto.precio
+    carrito.precioTotalReal += producto.precioReal
+    carrito.cantidadDeProductos++
+    cantidadDeProductosHTML.innerHTML = carrito.cantidadDeProductos
     carritoHTML.innerHTML +=`
     <div id="${idDelCarrito}" class="d-flex flex-wrap">
     <hr class="col-12">
@@ -56,32 +78,44 @@ const agregarAlCarrito = (id) => {
     <hr class="col-12">
     </div>
     `
-    totalSteamHTML.innerHTML = precioTotal
-    totalRealHTML.innerHTML = precioTotalReal
+    carrito.carritoDisplay=carritoHTML.innerHTML
+    totalSteamHTML.innerHTML = carrito.precioTotal
+    totalRealHTML.innerHTML = carrito.precioTotalReal
     alertPlaceholder.innerHTML = `<div class="alert alert-success alert-dismissible fixed-bottom" role="alert"> Agregaste ${producto.juego} al carrito! <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>`
+    const carritoJSON = JSON.stringify(carrito)
+    localStorage.setItem("carrito",carritoJSON)
 }
 
 const sacarDelCarrito = (id, idCarrito) => {
     const producto = productos.find(element => element.id == id)
-    precioTotal -= producto.precio
-    precioTotalReal -= producto.precioReal
-    cantidadDeProductosHTML.innerHTML--
-    document.getElementById(idCarrito).innerHTML=null
-    totalSteamHTML.innerHTML = precioTotal
-    totalRealHTML.innerHTML = precioTotalReal
+    carrito.precioTotal -= producto.precio
+    carrito.precioTotalReal -= producto.precioReal
+    carrito.cantidadDeProductos--
+    cantidadDeProductosHTML.innerHTML = carrito.cantidadDeProductos
+    document.getElementById(idCarrito).outerHTML=null
+    carrito.carritoDisplay=carritoHTML.innerHTML
+    totalSteamHTML.innerHTML = carrito.precioTotal
+    totalRealHTML.innerHTML = carrito.precioTotalReal
+    const carritoJSON = JSON.stringify(carrito)
+    localStorage.setItem("carrito",carritoJSON)
 }
 
+
 const comprar = () => {
-    if(precioTotalReal===0){
-        alert(`Dale ${nombre}, compra algo`)
+    if(carrito.precioTotalReal===0){
+        document.getElementById("modal").innerHTML = "No compraste nada gato"
     }else{
-        alert(`Felicidades ${nombre}, compraste jueguitos, gastaste $${precioTotalReal} de veras`)
+        document.getElementById("modal").innerHTML = `Felicidades, compraste jueguitos y gastaste $${carrito.precioTotalReal}`
         carritoHTML.innerHTML=null
         cantidadDeProductosHTML.innerHTML=0
+        carrito.cantidadDeProductos = 0
         totalSteamHTML.innerHTML = 0
         totalRealHTML.innerHTML = 0
-        precioTotal=0
-        precioTotalReal=0
+        carrito.carritoDisplay=carritoHTML.innerHTML
+        carrito.precioTotal=0
+        carrito.precioTotalReal=0
+        const carritoJSON = JSON.stringify(carrito)
+        localStorage.setItem("carrito",carritoJSON)
     }
 }
 
@@ -101,17 +135,38 @@ const ponerProductosEnHTML = (objeto) => {
     objeto.forEach(element =>{
         acumuladorDeHTML += `<div id="${element.id}" class="col-3 d-flex flex-wrap border border-dark border-3 rounded-2 p-3">
             <h2 class="row-4 col-12">${element.juego}</h2>
+            <img src="${element.foto}" alt="foto de ${element.juego}" style=" height:200px; width:100%; object-fit:contain">
             <div class="row-4 col-12">
                 <h4>precio Steam: ${element.precio}</h4>
                 <h4>precio real: ${element.precioReal}</h4>
             </div>
-            <button type="button" class="btn btn-primary row-4 col-12 align-self-end" onclick="agregarAlCarrito(${element.id})">Agregar</button>
+            <button type="button" class="btn btn-primary row-2 col-12 align-self-end my-2" onclick="agregarAlCarrito(${element.id})">Agregar</button>
+            <button type="button" class="btn btn-primary row-2 col-12 align-self-end" onclick="visitarPagina(${element.id})" data-bs-toggle="modal" data-bs-target="#juegoModal">Ver</button>
         </div>`
     })
+    document.getElementById("saludo").innerHTML=`<h1 class="text-center">SteameAR</h1>`
     productosHTML.innerHTML=acumuladorDeHTML
+    cantidadDeProductosHTML.innerHTML = carrito.cantidadDeProductos
+    carritoHTML.innerHTML = carrito.carritoDisplay
+    totalSteamHTML.innerHTML = carrito.precioTotal
+    totalRealHTML.innerHTML = carrito.precioTotalReal
 }
 
 /*Termina crear la pagina al iniciar */
+
+
+/*Abrir modal con juego*/
+function visitarPagina(id){
+    let juegoAMostrar = productos.find(e=>e.id == id)
+    document.getElementById("juegoModalLabel").innerHTML=juegoAMostrar.juego
+    document.getElementById("juegoModalFoto").innerHTML=`<img src="${juegoAMostrar.foto}" alt="foto de ${juegoAMostrar.juego}" class="w" style=" max-height:300px; width:100%; object-fit:contain">`
+    document.getElementById("juegoModalDesc").innerHTML=`<h4>$${juegoAMostrar.precio} - $${juegoAMostrar.precioReal}</h4>`+juegoAMostrar.descripcion+`<div>Link de steam: <a href="${juegoAMostrar.link}" target="_blank">${juegoAMostrar.juego}</a></div>`
+    document.getElementById("juegoModalFooter").innerHTML=`
+    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+    <button type="button" class="btn btn-primary" onclick="agregarAlCarrito(${juegoAMostrar.id})" data-bs-dismiss="modal">Comprar</button>`
+}
+
+/*Termina abrir modal con juego*/
 
 
 /*Ordenar productos*/
@@ -214,8 +269,6 @@ botonBorrarFiltro.onclick = () => {
 
 /*Termina filtro de precios*/
 
-
-document.getElementById("saludo").innerHTML=`<h1 class="text-center">Bienvenido ${nombre}!</h1>`
 
 agregarPreciosReales()
 ponerProductosEnHTML(productos)
